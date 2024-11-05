4db6deb8dff401a4f5a5d4c9ca9f512dc0fb76de
luckfish <luckfish@localhost.localdomain>
2024-11-05 00:07:46 -0300

    progress_bar

--- script.sh
+++ script.sh
@@ -14,15 +14,19 @@ path=$1
 theme="/boot/grub/theme/"
 config="/etc/default/grub"
 
+source progress_bar.sh
+setup_scroll_area
 if [ -d "$theme" ]; then
     echo "Directory theme exist"
+    draw_progress_bar 35
 else
     echo "Creating directory theme"
     mkdir $theme
+    draw_progress_bar 25
 if [ "$?" -ne 0 ]; then 
     echo "Impossible created directory"
     exit 1
-fi
+fi    
 fi
 
 if [ $(echo $1 | awk '{print substr($0, 1, 1)}') != '/' ]; then  
@@ -30,8 +34,8 @@ if [ $(echo $1 | awk '{print substr($0, 1, 1)}') != '/' ]; then
 fi
 
 echo "Copying files to destination..."
-
 cp -r $path $theme
+draw_progress_bar 50
 
 for i in $(ls $theme -tc)
   do
@@ -43,11 +47,19 @@ theme=$theme$name_dir
 #path final do theme
 theme=$(find $theme -name theme.txt)
 
+echo "Update file Grup configuration"
+draw_progress_bar 80
+
 #remove o grub_theme usar -i pra moldar o arquivo
 sed -i '/^GRUB_THEME/d' $config
 echo "GRUB_THEME="$theme >> $config
 
 #$grub-mkconfig -o /boot/grub/grub.cfg
 echo "Upgradeble grub"
+draw_progress_bar 90
 $(update-grub2)
+
+sleep 1
 echo "Finish"
+draw_progress_bar 100
+destroy_scroll_area
--- test.sh
+++ test.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 #theme="/boot/grub/theme"
 #t="/etc/default/grub"
 #name_dir=$(ls $theme -tc)
@@ -15,7 +17,13 @@
 #sed "/theme/! echo foi" "/home/luckfish/codigos/codigos-c/shell/test.sh"
 #
 #sed "/^GRUB_THEEME/
+#
+source progress_bar.sh
+setup_scroll_area
 for i in $(seq 0 10 100)
 do
         echo "$i"
+        draw_progress_bar $i
+        sleep 1
 done
+destroy_scroll_area
